5d62f7eede244691a11f85c161202045cadb3760
Oscar Blanco <oscarmb86@hotmail.com>
2025-01-22 23:58:18 -0600

    added functionality

--- 04b-calculator-app/hooks/Calculator.tsx
+++ 04b-calculator-app/hooks/Calculator.tsx
@@ -18,7 +18,7 @@ export const useCalculator = () => {
     const buildNumber = (numberString: string) => {
         console.log({numberString, number});
         //Verificar si ya existe el punto decimal
-        if (number.includes('.') && numberString === '.') return;
+        if (number.includes('.') numberStrin&& g === '.') return;
         if (number.startsWith('0') || number.startsWith('-0')) {
             if (numberString === '.') setNumber(number + numberString);
         //     if (numberString ==='0' && number.includes('.')) setNumber(number + numberString );
@@ -28,7 +28,14 @@ export const useCalculator = () => {
         setNumber(number + numberString);
         // console.log({number});
     };
-    const clearNumber = () => setNumber('0');
+    const clearNumber = () => {
+        setNumber('0')
+        setPrevNumber('0');
+        setFormula('0');
+    };
+    // const toggleSign = (num) => {
+
+    // }
     const deleteNumber = () => setNumber(number.slice(0,1));
     return {
         // props
