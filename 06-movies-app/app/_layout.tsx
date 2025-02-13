09d370f1f2702d99805def7b81d6006b5fc900d6
Oscar Blanco <oscarmb86@hotmail.com>
2025-02-13 06:54:42 -0600

    started the movies app and tested the api with axios

--- 06-movies-app/app/_layout.tsx
+++ 06-movies-app/app/_layout.tsx
@@ -1,6 +1,14 @@
-import { Slot } from "expo-router";
+import { View, Text } from 'react-native'
+import React from 'react'
+// import { nowPlayingAction } from '@/core/actions/movies/now-playing.action'
 
-// Import your global CSS file
-import "../global.css";
+const RootLayout = () => {
+  // nowPlayingAction()
+  return (
+    <View>
+      <Text className='text-3xl'>RootLayout</Text>
+    </View>
+  )
+}
 
-export default Slot;
+export default RootLayout 
---
+++ 06-movies-app/core/actions/movies/now-playing.action.ts
@@ -0,0 +1,16 @@
+import { MovieDBMoviesResponse } from "@/infraestructure/interface/moviedb-response"
+import { movieApi } from "./api/movie-api"
+import { MovieMapper } from "@/infraestructure/interface/mappers/movie.mapper"
+
+export const nowPlayingAction = async() => {
+    try {
+        const { data } = await movieApi.get<MovieDBMoviesResponse>('/now_playing')
+        // console.log(JSON.stringify(data,null,2))
+        const movies = data.results.map(MovieMapper.fromTheMovieDBToMovie)
+        // console.log(movies)
+        return movies
+    } catch(error) {
+        console.log(error)
+        throw 'Cannot load now playin movies'
+    }
+}
