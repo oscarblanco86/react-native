2c3b2cb33bf7fd168426d755f891b0c9743fcb62
Oscar Blanco <oscarmb86@hotmail.com>
2025-03-01 23:36:29 -0600

    Creado flat list para generar un infinit scroll

--- 06-movies-app/presentation/components/movies/MovieHorizontalList.tsx
+++ 06-movies-app/presentation/components/movies/MovieHorizontalList.tsx
@@ -1,5 +1,5 @@
-import { View, Text, FlatList } from 'react-native'
-import React from 'react'
+import { View, Text, FlatList, NativeSyntheticEvent, NativeScrollEvent } from 'react-native'
+import React, { useRef } from 'react'
 import { Movie } from '@/infraestructure/interface/movie.interface'
 import MoviePoster from './MoviePoster'
 
@@ -7,8 +7,27 @@ interface Props {
   title: string
   movies: Movie[]
   className?: string
+  loadNextPage?: () => void  
 }
-const MovieHorizontalList = ({ movies, title, className }: Props) => {
+const MovieHorizontalList = ({ movies, title, className, loadNextPage}: Props) => {
+  const isLoading = useRef(false)
+  
+  const onScroll = (event: NativeSyntheticEvent<NativeScrollEvent>) => {
+    if (isLoading.current) return
+
+    const {contentOffset, layoutMeasurement, contentSize} = event.nativeEvent
+
+    const isEndReached = (contentOffset.x + layoutMeasurement.width + 600) >= contentSize.width
+
+    if (!isEndReached) return
+
+    isLoading.current = true
+
+    console.log('Cargar siguientes peliculas')
+
+    loadNextPage && loadNextPage()
+    
+  }
   return (
     <View className={`${className}`}>
       <Text className='text-3xl font-bold px-4 mb-2'>{title}</Text>
@@ -24,6 +43,7 @@ const MovieHorizontalList = ({ movies, title, className }: Props) => {
             smallPoster
           />
         }
+        onScroll={onScroll}
       />
     </View>
   )
