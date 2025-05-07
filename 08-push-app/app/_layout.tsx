b1fef09375878506b23f9d7536b797269544e3bf
Oscar Blanco <oscarmb86@hotmail.com>
2025-05-07 11:23:24 -0600

    working with the minimal working example

--- 08-push-app/app/_layout.tsx
+++ 08-push-app/app/_layout.tsx
@@ -5,9 +5,11 @@ import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { useThemeColor } from '@/hooks/useThemeColor';
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+  const backgroundColor = useThemeColor({},'background')
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
@@ -19,9 +21,22 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
+      <Stack
+        screenOptions={{
+          headerShadowVisible: false,
+          contentStyle: {
+            backgroundColor: backgroundColor
+          }
+
+        }}
+
+      >
+        <Stack.Screen 
+          name='index'
+          options={{
+            title: 'Push App'
+          }}
+        />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
