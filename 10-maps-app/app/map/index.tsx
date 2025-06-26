e4347226205d66f301cdfa640a7a08cc7cda8d61
Oscar Blanco <oscarmb86@hotmail.com>
2025-06-26 12:25:50 -0600

    testing prebuild of the project

--- 10-maps-app/app/map/index.tsx
+++ 10-maps-app/app/map/index.tsx
@@ -1,17 +1,33 @@
-import { ThemedText } from '@/presentation/components/shared/ThemedText'
-import React from 'react'
-import { View } from 'react-native'
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import MapView from "react-native-maps";
 
 const MapScreen = () => {
   return (
-    <View style={{
-        flex:1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-      <ThemedText>MapScreen</ThemedText>
-    </View>
-  )
-}
+    <View style={styles.container}>
+      <MapView
+        showsPointsOfInterest={false}
+        style={styles.map}
+        initialRegion={{
+          // latitude: 37.78825,
+          // longitude: -122.4324,
+          latitude: 14.769144,
+          longitude: -88.783625,
 
-export default MapScreen
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: "100%",
+    height: "100%",
+  },
+});
+export default MapScreen;
