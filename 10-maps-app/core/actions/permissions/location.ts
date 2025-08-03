d06242c1c9870d158929d2249bbb6d7452e07126
Oscar Blanco <oscarmb86@hotmail.com>
2025-08-02 22:23:41 -0600

    restarting last project of class

--- 10-maps-app/core/actions/permissions/location.ts
+++ 10-maps-app/core/actions/permissions/location.ts
@@ -1,49 +1,14 @@
 import * as Location from 'expo-location'
 
-import { PermissionStatus } from "@/infrastructure/interfaces/location"
-import { Alert, Linking } from 'react-native'
-
-
-export const requestLocationPermission = async():Promise<PermissionStatus> => {
-    const {status} = await Location.requestForegroundPermissionsAsync()
-
-    if (status !== 'granted') {
-        if (status === 'denied') {
-            manualPermissionRequest()
-        }
-        return PermissionStatus.DENIED
-    }
-    return PermissionStatus.GRANTED
+export const requestLocationPermission = async() => {
+    Location
 
 }
 
 export const checkLocationPermission = async() => {
-    const {status} = await Location.getForegroundPermissionsAsync();
-    switch(status) {
-        case 'granted':
-            return PermissionStatus.GRANTED
-        case 'denied':
-            return PermissionStatus.DENIED
-        default:
-            return PermissionStatus.UNDETERMINED
-    }
+
 }
 
- const manualPermissionRequest = async() => {
-    Alert.alert(
-        'Permiso de ubicación necesario',
-        'Para continuar debe de habilitar el permiso de location en los ajustes de la app',
-        [
-            {
-                text: 'Abrir ajustes',
-                onPress: () => {
-                    Linking.openSettings()
-                }
-            },
-            {
-                text: 'Cancel',
-                style: 'destructive'
-            }
-        ]
-    )
+export const manualPermissionRequest = async() => {
+
 }
--- 10-maps-app/presentation/components/shared/ThemedText.tsx
+++ 10-maps-app/presentation/components/shared/ThemedText.tsx
@@ -1,6 +1,6 @@
 import { StyleSheet, Text, type TextProps } from 'react-native';
 
-import { useThemeColor } from '@/presentation/hooks/useThemeColor';
+import { useThemeColor } from './../../hooks/useThemeColor';
 
 export type ThemedTextProps = TextProps & {
   lightColor?: string;
@@ -20,7 +20,7 @@ export function ThemedText({
   return (
     <Text
       style={[
-        { color },
+       
         type === 'default' ? styles.default : undefined,
         type === 'title' ? styles.title : undefined,
         type === 'defaultSemiBold' ? styles.defaultSemiBold : undefined,
