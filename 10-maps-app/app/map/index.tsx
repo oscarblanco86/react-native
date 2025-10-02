42ea378036026490ad6076f1cc1bd71f66c6d639
Oscar Blanco <oscarmb86@hotmail.com>
2025-10-01 23:48:41 -0600

    added changes to the maps view

--- 10-maps-app/app/map/index.tsx
+++ 10-maps-app/app/map/index.tsx
@@ -1,12 +1,24 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 
 const MapScreen = () => {
   return (
-    <View>
-      <Text>MapScreen</Text>
+    <View style={styles.container}>
+      <View style={styles.map}></View>
+      {/* <Text>MapScreen</Text> */}
     </View>
   )
 }
 
 export default MapScreen
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: "100%",
+    height: "100%",
+    backgroundColor: 'red'
+  }
+})
